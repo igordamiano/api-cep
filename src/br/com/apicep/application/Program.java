c52131eb6a66bfef3079f12444402a313f86d7df
Igor <igordamiano@gmail.com>
2020-03-21 12:58:00 -0300

    api consome cep json

---
+++ src/br/com/apicep/application/Program.java
@@ -0,0 +1,20 @@
+package br.com.apicep.application;
+
+import br.com.apicep.domain.Endereco;
+import br.com.apicep.service.ServicoDeCep;
+
+public class Program {
+
+	public static void main(String[] args) throws Exception {
+
+		String cep = "05367080";
+		
+		Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
+		
+		System.out.println("Logradouro: " + endereco.getLogradouro());
+		System.out.println("Bairro: " + endereco.getBairro());
+		System.out.println("Localidade: " + endereco.getLocalidade());
+		
+	}
+
+}
